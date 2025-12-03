f7e340b1690685e8df1c48046a9871ce52470146
Artxe Web Diseño web Ponferrada SEO <117656534+artxewebponferrada@users.noreply.github.com>
2025-12-03 12:18:35 +0100

    Delete src/react-wrappers directory

--- src/react-wrappers/RadarIslandWrapper.tsx
+++
@@ -1,7 +0,0 @@
-import RadarIsland from "@/components/islands/RadarIsland.astro";
-
-export default function RadarIslandWrapper({ scores }: { scores: any }) {
-  return (
-    <RadarIsland scores={scores} client:load />
-  );
-}
